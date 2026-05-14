a421b73778c2fbcca367d30c863bfb3adf8ed03d
lil-aditya <adg.aids.iitj@gmail.com>
2026-05-14 08:05:55 +0530

    fix: replace non-standard bits/stdc++.h with specific includes
    bits/stdc++.h is a GCC-internal header that clangd and most IDEs
    cannot resolve, causing false errors. Replaced with the 4 specific
    standard headers the Graph class actually needs: map, vector,
    queue, iostream. Also removed 'using namespace std' from header
    and added proper std:: prefixes.

--- include/graph_routing.hpp
+++ include/graph_routing.hpp
@@ -1,15 +1,17 @@
 #pragma once
-#include <bits/stdc++.h>
-using namespace std;
+#include <map>
+#include <vector>
+#include <queue>
+#include <iostream>
 
 class Graph {
 private:
-    map<int,vector<int>> adjList;
+    std::map<int, std::vector<int>> adjList;
     int numNodes;
 public:
     Graph(int nodes);
     void addEdge(int u, int v);
     void BFS(int startNode);
 
-    vector<int> findShortestPath(int startNode, int endNode);
+    std::vector<int> findShortestPath(int startNode, int endNode);
 };
